9b3d876da4b628154d6fc2cb52b26226077cf6b2
rschwenck <rodrigo.schwenck@gmail.com>
2026-09-01 15:23:09 +0000

    Execução do exercicio

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -5,7 +5,18 @@
 
 # Declare variáveis do tipo string
 
+nome = 'Rodrigo'
+cidade = 'Santo André'
+uf = 'São Paulo'
+
 # Declare variáveis de cada tipo numérico
+
+idade = 45
+peso = 94
+altura = 1,81
 
 
 # Declare uma variável do tipo booleano
+
+verdade = True
+mentira = False
